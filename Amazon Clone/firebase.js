112dc517002e423bffdf883917dc1774f4ecf09f
Resh_nbs <61882829+ReshmiSeetha@users.noreply.github.com>
2021-08-17 11:42:39 +0530

    Add files via upload

---
+++ Amazon Clone/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBuj-zNi0GuYNv6CFnrZsXMXa6cfHLBML8",
+    authDomain: "clone-d61da.firebaseapp.com",
+    projectId: "clone-d61da",
+    storageBucket: "clone-d61da.appspot.com",
+    messagingSenderId: "941032136199",
+    appId: "1:941032136199:web:56550ac5cb56e4c56b54a2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var db = firebase.firestore();
